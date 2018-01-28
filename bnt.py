7462f56db1e6951265a5e0c443c0862dc153360b
David Stolarsky <david.stolarsky@gmail.com>
2018-01-27 22:53:23 -0500

    save db on ctrl c

--- bnt.py
+++ bnt.py
@@ -10,30 +10,50 @@ result = c.fetchone()
 if result == None:
     print("Creating table...")
     c.execute('''CREATE TABLE outputs
-                 (tx_hash text, output_number int, address text)''')
+                 (tx_hash text, output_number int, address text, amount text)''')
     c.execute('CREATE UNIQUE INDEX tx_output ON outputs (tx_hash, output_number);')
 
+def commit_db_and_exit():
+    conn.commit()
+    conn.close()
+    exit()
+
+import signal
+def signal_handler(signal, frame):
+        print('\nReceived SIGINT, commiting DB and quitting...')
+        commit_db_and_exit()
+
+signal.signal(signal.SIGINT, signal_handler)
+
+insertions_since_last_commit = 0
 def store_output(tx_hash, output_number, _output):
+    global insertions_since_last_commit
     # don't know how to handle outputs mentioning more than 1 address at the moment
     if len(_output.addresses) == 1:
-        c.execute('INSERT INTO outputs VALUES(?, ?, ?)', (tx_hash, output_number, _output.addresses[0].address))
+        c.execute('INSERT INTO outputs VALUES(?, ?, ?, ?)', (tx_hash, output_number, _output.addresses[0].address, _output.value))
+        insertions_since_last_commit = insertions_since_last_commit + 1
+        if insertions_since_last_commit > 1000:
+            conn.commit()
+            insertions_since_last_commit = 0
     elif len(_output.addresses) == 0:
         print("Info: 0 address output. tx=%s output_number=%s output=%s" % (tx_hash, output_number, _output))
         if _output.value != 0:
             print("...of value = %s satoshis" % (_output.value))
             print("...quitting, we should handle this right?")
-            exit()
+            commit_db_and_exit()
         else:
             print("...of value 0")
     else:
         print("multi address output")
         print(_output.addresses)
-        exit()
+        commit_db_and_exit()
 
-def lookup_address_of_input(_input):
+def lookup_input(_input):
     # print(_input)
-    c.execute('SELECT address FROM outputs WHERE tx_hash=? AND output_number=?', (_input.transaction_hash, _input.transaction_index))
-    print(c.fetchone())
+    c.execute('SELECT address, amount FROM outputs WHERE tx_hash=? AND output_number=?', (_input.transaction_hash, _input.transaction_index))
+    result = c.fetchone()
+    if result != None:
+        print("Found %s" % (result,))
 
 # Instantiate the Blockchain by giving the path to the directory 
 # containing the .blk files created by bitcoind
@@ -50,7 +70,7 @@ for block in blockchain.get_unordered_blocks():
         # tx=635745100dc559787dc3b09bd31a62155084dc582aff741debb18828b91a8ec0 input_num=0 input=Input(66f701374a05702aa1ab920486cfc1b7a1f643b6ed75c04f2583d412f12ff88b,2)
 
         for no, _input in enumerate(tx.inputs):
-            input_address = lookup_address_of_input(_input)
+            input_address = lookup_input(_input)
             # print("tx=%s input_num=%s input=%s" % (tx.hash, no, _input))
 
         # so, 1 or more outputs may be change wallets, wallets "secretly"
@@ -67,4 +87,4 @@ for block in blockchain.get_unordered_blocks():
         
         for no, output in enumerate(tx.outputs):
             store_output(tx.hash, no, output)
-            print("tx=%s outputno=%d type=%s value=%s" % (tx.hash, no, output.addresses, output.value))
+            # print("tx=%s outputno=%d type=%s value=%s" % (tx.hash, no, output.addresses, output.value))
